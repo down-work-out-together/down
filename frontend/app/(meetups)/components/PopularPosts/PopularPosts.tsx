7f81c383c32feecd6ae4479546bde3b22e0d85ba
Jiwon Park <xoxojw.dev@gmail.com>
2024-03-07 23:50:39 +0900

    design: #87 모임 페이지 UI 컴포넌트 구현

--- frontend/app/(meetups)/components/PopularPosts/PopularPosts.tsx
+++ frontend/app/(meetups)/components/PopularPosts/PopularPosts.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { dummyPosts } from "../dummyPosts";
-import { PostItem } from "@/app/(meetups)/components";
+import { MeetUpPageSection, PostItem } from "@/app/(meetups)/components";
 
 const PopularPosts = () => {
 	const popularPosts = dummyPosts.slice(0, 4);
 	return (
-		<section className="w-full flex flex-col my-16">
-			<div className="flex flex-col max-w-[70rem] mx-auto space-y-8">
-				<h1 className="text-2xl font-semibold">이번 주 대세 모임</h1>
-				<div className="w-full flex justify-center gap-x-8">
-					{popularPosts.map((post, index) => (
+		<MeetUpPageSection sectionTitle="이번 주 대세 모임">
+			<div className="w-full grid lg:grid-cols-4 gap-x-4 grid-cols-1">
+				{popularPosts.map((post, index) => (
+					<div
+						key={index}
+						className="flex justify-center items-center"
+					>
 						<PostItem
 							key={index}
 							post={post}
 						/>
-					))}
-				</div>
+					</div>
+				))}
 			</div>
-		</section>
+		</MeetUpPageSection>
 	);
 };
 
--- frontend/app/(meetups)/components/PostItem/PostItem.tsx
+++ frontend/app/(meetups)/components/PostItem/PostItem.tsx
@@ -11,8 +11,8 @@ type PostTypeProps = {
 
 const PostItem = ({ post }: PostTypeProps) => {
 	return (
-		<div className="w-64 space-y-2">
-			<div className="w-64 h-[11.25rem] bg-zinc-200 rounded-md" />
+		<div className="w-full max-w-96 lg:max-w-64 space-y-2 py-2 cursor-pointer hover:-translate-y-2 border-stone-400 transition-transform">
+			<div className="w-full max-w-96 lg:max-w-64 h-[11.25rem] bg-stone-200 rounded-md" />
 			<h3 className="truncate">{post.title}</h3>
 			<p className="text-sm">{post.location}</p>
 			<p className="text-sm">인원: {post.numberOfParticipants}</p>
